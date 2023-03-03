b3ed9ffaddea5b29d4f00d444c12ebd5157af271
imbatony <esfak47@qq.com>
2023-03-03 11:50:48 +0800

    0.0.5

--- Formula/AVOneTool.rb
+++ Formula/AVOneTool.rb
@@ -7,7 +7,7 @@ class AVOneTool < Formula
   on_macos do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
       url "https://github.com/weloveloli/AVOne/releases/download/tool-0.0.5/AVOneTool.0.0.5.osx-x64.tar.gz"
-      sha256 "5e0736998fd337c936ad6ed459fabe070cdb66a997d8eafbd2d6ea63be764f5a"
+      sha256 "765d5a8eaf69afb05fe92d7f5d995b70eba25a139e3ba713d379f0d270cb8ff2"
 
       def install
         cp_r '.', bin
@@ -16,7 +16,7 @@ class AVOneTool < Formula
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/weloveloli/AVOne/releases/download/tool-0.0.5/AVOneTool.0.0.5.osx-arm64.tar.gz"
-      sha256 "fcc26699f3cdf12c8ff7a43ad553f322d2d4cacd25274bc6c6ea7042e7ccdbb1"
+      sha256 "d89ba28d2acaa656e1bb24de765b04d69d36854f0f19f9ad64894f71d0afb3fa"
 
       def install
         cp_r '.', bin
@@ -27,7 +27,7 @@ class AVOneTool < Formula
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
       url "https://github.com/weloveloli/AVOne/releases/download/tool-0.0.5/AVOneTool.0.0.5.linux-x64.tar.gz"
-      sha256 "55f1160e7cc06a8d5730aca75b1a42dd23717e0469cbf082d5f0fb6f96dfd276"
+      sha256 "fd3a1ff97add142d353e16d6768491745bceeb11c594e22577db3b4520e5974d"
 
       def install
         cp_r '.', bin
@@ -36,7 +36,7 @@ class AVOneTool < Formula
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_32_bit?
       url "https://github.com/weloveloli/AVOne/releases/download/tool-0.0.5/AVOneTool.0.0.5.linux-arm.tar.gz"
-      sha256 "9be9a075e33d3036428e36d03b7bc00608e33b15a3f013d63d55716217c56ecf"
+      sha256 "01da245bbe5a19862e8a0bdf66d5d0f9fcfec0eed27f9a320d2a1c5015c38009"
 
       def install
         cp_r '.', bin
@@ -45,7 +45,7 @@ class AVOneTool < Formula
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/weloveloli/AVOne/releases/download/tool-0.0.5/AVOneTool.0.0.5.linux-arm64.tar.gz"
-      sha256 "94c75ff8881603be7d277f6b1dd2ccb4585f9fee3e4cbda0217cad8f111ba931"
+      sha256 "21c8089b8455b2558c8bfb6a5da3a5a644311573bdced8f55cfbc7e07997e493"
 
       def install
         cp_r '.', bin
